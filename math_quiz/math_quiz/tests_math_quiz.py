28c1fd7c25de0165e6c87e88019dd756413350f8
prathameshagare <prathameshagare137@gmail.com>
2023-11-13 16:51:14 +0100

    Added unit tests

--- math_quiz/math_quiz/tests_math_quiz.py
+++ math_quiz/math_quiz/tests_math_quiz.py
@@ -1,30 +1,34 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import generate_random_integer, generate_random_operator, calculate_result
 
+class TestMathQuizFunctions(unittest.TestCase):
 
-class TestMathGame(unittest.TestCase):
-
-    def test_function_A(self):
-        # Test if random numbers generated are within the specified range
+    def test_generate_random_integer(self):
+        # Test if random integers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = generate_random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_generate_random_operator(self):
+        # Test if the generated operator is one of the valid operators
+        valid_operators = ['+', '-', '*']
+        for _ in range(1000):  # Test a large number of random values
+            rand_operator = generate_random_operator()
+            self.assertIn(rand_operator, valid_operators)
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_calculate_result_addition(self):
+        result = calculate_result(5, 2, '+')
+        self.assertEqual(result, 7)
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+    def test_calculate_result_subtraction(self):
+        result = calculate_result(5, 2, '-')
+        self.assertEqual(result, 3)
+
+    def test_calculate_result_multiplication(self):
+        result = calculate_result(5, 2, '*')
+        self.assertEqual(result, 10)
 
 if __name__ == "__main__":
     unittest.main()
